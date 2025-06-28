2b36b2de2641ce24fc0dd9a199c1baf7605001d5
Naveen Kumar <knaveenk869@gmail.com>
2025-06-28 10:08:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function firstChar(text) {
   // your code here
 	for(let i=0;i<text.length;i++){
 		if(text[i] !== ''){
-			return text[i];
+			console.log(text[i]);
+			break;
 		}
 	}
-	return '';
+	console.log('';)
 }
 
 // Do not change the code below
